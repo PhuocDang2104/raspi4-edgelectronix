32c5d4635db37c86e70aee8089865ebe89b41bd6
PhuocDang2104 <phuoc.dang2104@gmail.com>
2025-08-21 22:37:48 +0700

    update và rename

--- web-flask/run_emit.py
+++ web-flask/run_emit.py
@@ -1,5 +1,5 @@
 # run_emit.py
-from celery_tasks.emit import run_emit_loop
+from services.emit import run_emit_loop
 
 if __name__ == '__main__':
     run_emit_loop()
---
+++ web-flask/services/udp_server.py
@@ -0,0 +1,64 @@
+import redis
+import socket
+import time
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+UDP_IP = "::"
+UDP_PORT = 12345
+
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print("📡 Listening on UDP port", UDP_PORT)
+
+# Gán sẵn IPv6
+addr1_ip = "fdf3:907:f99e:9fa1:e09a:2d59:dd67:b31"
+addr2_ip = "fdf3:907:f99e:9fa1:4bca:b459:e447:40b1"
+
+while True:
+    sock.settimeout(0.5)
+    try:
+        data, addr = sock.recvfrom(1024)
+        message = data.decode().strip()
+        
+        
+
+        if addr1_ip is None or addr[0] == addr1_ip:
+            addr1_ip = addr[0]
+            redis_client.set('selected_perfume_id_from_udp', message)
+            print(f"* Set Redis key 'selected_perfume_id_from_udp': {message}")
+
+        elif addr2_ip is None or addr[0] == addr2_ip:
+            addr2_ip = addr[0]
+            if message.upper().startswith("TEMP:"):
+                try:
+                    parts = message.split(",")
+                    temp_str = parts[0].replace("Temp=", "").replace("C", "")
+                    hum_str = parts[1].replace("Hum=", "").replace("%", "")
+                    
+                    # Chuyển sang float
+                    temperature = float(temp_str)
+                    humidity = float(hum_str)
+                    # Lưu vào Redis
+                    redis_client.hset('environment_monitor', mapping={
+                        'temperature': temperature,
+                        'humidity': humidity
+                    })
+
+                    print(f"-- Nhiệt độ: {temperature}°C | Độ ẩm: {humidity}%")
+                    
+                except Exception as e:
+                    print(f"Lỗi parse message '{message}': {e}")
+                    
+            elif message.lower() == "drop":
+                redis_client.set('drop_detect_event', "1")
+                print(f"-- Phát hiện đỗ vỡ AI event: {message}")
+
+        else:
+            print("⚠️ Unknown device, both slots full. Ignoring.")
+
+    except socket.timeout:
+        pass
+
+    time.sleep(0.2)
