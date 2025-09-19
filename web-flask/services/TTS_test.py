e1b32bead090f6eb9a253e52731c4d8afb99305e
PhuocDang2104 <phuoc.dang2104@gmail.com>
2025-09-19 14:46:12 +0700

    TTS fixed

--- web-flask/services/TTS_test.py
+++ web-flask/services/TTS_test.py
@@ -1,4 +1,8 @@
 import os
 
-text = "Hello, this is a clearer test with e-speak NG."
-os.system(f'espeak-ng "{text}" -s 150 -a 200 -v en+f3')
+text = "Hello, this is a test of speech synthesis on Raspberry Pi."
+# -v en+f3: chọn giọng nữ tiếng Anh
+# -s 150: tốc độ vừa phải
+# -a 200: max volume
+cmd = f'espeak "{text}" -s 150 -a 200 -v en+f3'
+os.system(cmd)
