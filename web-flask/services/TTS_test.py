4c5076fa52580c4b3496deaad486aadf79fc7b2d
PhuocDang2104 <phuoc.dang2104@gmail.com>
2025-09-19 14:44:32 +0700

    ENG

--- web-flask/services/TTS_test.py
+++ web-flask/services/TTS_test.py
@@ -1,7 +1,4 @@
 import os
 
-# Text to speak
-text = "Hello, this is a test of speech synthesis on Raspberry Pi."
-
-# Use espeak to play
-os.system(f'espeak "{text}" -s 150 -a 200 -v en+f3')
+text = "Hello, this is a clearer test with e-speak NG."
+os.system(f'espeak-ng "{text}" -s 150 -a 200 -v en+f3')
